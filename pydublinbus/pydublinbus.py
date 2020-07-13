f2afea26351488073443763b5d3f40b7de751755
Notebook <admin@ialex.info>
2020-07-13 22:59:16 +0100

    Add filtration by bus route

--- pydublinbus/pydublinbus.py
+++ pydublinbus/pydublinbus.py
@@ -2,56 +2,67 @@
 Dublin Bus RTPI REST API client
 """
 
-from datetime import datetime, timedelta
 import json
 import requests
 
 _RESOURCE = "https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation"
 
+
+class ConnectionError(Exception):
+    pass
+
+
+class APIError(Exception):
+    pass
+
+
 class DublinBusRTPI(object):
-    def __init__(self, stopid=None):
+    def __init__(self, stopid=None, route=None):
         """
         A class used to interact with the Dublin Bus RTPI API
         
         :param stopid: Dublin bus stop id (default None).
         :type stopid: int
+        :param route: Dublin bus route number (default None)
+        :type route: int
 
         """
         self.stopid = stopid
+        self.route = route
 
-    def get_rtpi_data(self):
+    def raw_rtpi_data(self):
         """
         :returns: Return a json object containing Dublin Bus RTPI data
-        :rype: dict
+        :rtype: dict
         """
-        params = {
-            "stopid": self.stopid,
-            "format": "json"
-        }
-        
+        params = {"stopid": self.stopid, "format": "json"}
+
         try:
             response = requests.get(_RESOURCE, params, timeout=10)
-        
-        except (requests.ConnectionError,
-                requests.RequestException,
-                requests.HTTPError,
-                requests.Timeout,
-                requests.TooManyRedirects) as e:
-            raise self.CONNError(str(e))
-       
+
+        except (
+            requests.ConnectionError,
+            requests.RequestException,
+            requests.HTTPError,
+            requests.Timeout,
+            requests.TooManyRedirects,
+        ) as e:
+            raise ConnectionError(str(e))
+
         try:
             results = json.loads(response.text)
             results = response.json()
+
         except ValueError:
-            raise self.APIError('JSON parse failed.')
-        
-        if results["errorcode"] != "0":
-            raise self.APIError(results["errormessage"])
-        
+            raise APIError("JSON parse failed.")
+
+        if results["errorcode"] not in ["0", "1"]:
+            raise APIError(results["errormessage"])
+
         return results
-    
+
     def bus_timetable(self):
-        """   
+        """    
         :return: List of the dictioneries of the due bus routes.
         :rtype: list
          
@@ -61,14 +72,15 @@ class DublinBusRTPI(object):
          {'due_in': '13', 'route': '25A'},
          {'due_in': '21', 'route': '66'}]
         """
-        timetable=[]
-        result = self.get_rtpi_data()
-        for item in result["results"]:
+        timetable = []
+        raw_data = self.raw_rtpi_data()
+
+        for item in raw_data["results"]:
             duetime = item.get("duetime")
             route = item.get("route")
-            bus_data = {
-                "route": route,
-                "due_in": duetime
-            }
-            timetable.append(bus_data)
+            if not self.route:
+                timetable.append({"due_in": duetime, "route": route})
+            else:
+                if self.route == route:
+                    timetable.append({"due_in": duetime, "route": route})
         return timetable
--- setup.py
+++ setup.py
@@ -3,28 +3,22 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name='pydublinbus',
-      version='0.0.2',
-      description='Python library to get the real-time transport information (RTPI) for Dublin Bus',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      keywords='dublin bus RTPI',
-      author='Alex Iepuras',
-      author_email='iepuras.alex@gmail.com',
-      license='MIT',
-      url='https://pypi.org/project/pydublinbus',
-      download_url='https://pypi.org/project/pydublinbus',
-      platforms=["any"],
-      packages=find_packages(),
-      zip_safe=False,
-      python_requires='>=3.6',
-      install_requires=[
-          'requests',
-      ],
-      tests_requires=[
-          'tox',
-          'flake8',
-          'pylint',
-          'pytest'
-      ]
-     )
+setup(
+    name="pydublinbus",
+    version="0.0.3b",
+    description="Python library to get the real-time transport information (RTPI) for Dublin Bus",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="dublin bus RTPI",
+    author="Alex Iepuras",
+    author_email="iepuras.alex@gmail.com",
+    license="MIT",
+    url="https://github.com/ialex87/pydublinbus",
+    download_url="https://github.com/ialex87/pydublinbus",
+    platforms=["any"],
+    packages=find_packages(),
+    zip_safe=False,
+    python_requires=">=3.6",
+    install_requires=["requests",],
+    tests_requires=["flake8", "pylint", "pytest"],
+)
